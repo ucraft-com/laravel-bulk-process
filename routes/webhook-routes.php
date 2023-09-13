92ad444433e11963b8910b68f81d362e11e5c81c
Poghos <poghosboyajyan@gmail.com>
2023-09-13 18:51:10 +0400

    Updated route methods

--- routes/webhook-routes.php
+++ routes/webhook-routes.php
@@ -9,9 +9,9 @@ use Uc\BulkProcessManager\BulkActionHandlerInterface;
 Route::prefix('bulk-process')->name('bulk-process.')->group(function (Router $router): void {
     $router->post('/{entity}', [BulkActionHandlerInterface::class, 'handleCreate'])->name('create');
     $router->put('/{entity}', [BulkActionHandlerInterface::class, 'handleUpdate'])->name('update');
-    $router->put('/{entity}/make-visible', [BulkActionHandlerInterface::class, 'handleMakeVisible'])->name('make-visible');
-    $router->put('/{entity}/make-invisible', [BulkActionHandlerInterface::class, 'handleMakeInvisible'])->name('make-invisible');
-    $router->put('/{entity}/publish', [BulkActionHandlerInterface::class, 'handlePublish'])->name('publish');
-    $router->put('/{entity}/unpublish', [BulkActionHandlerInterface::class, 'handleUnpublish'])->name('unpublish');
+    $router->patch('/{entity}/make-visible', [BulkActionHandlerInterface::class, 'handleMakeVisible'])->name('make-visible');
+    $router->patch('/{entity}/make-invisible', [BulkActionHandlerInterface::class, 'handleMakeInvisible'])->name('make-invisible');
+    $router->patch('/{entity}/publish', [BulkActionHandlerInterface::class, 'handlePublish'])->name('publish');
+    $router->patch('/{entity}/unpublish', [BulkActionHandlerInterface::class, 'handleUnpublish'])->name('unpublish');
     $router->delete('/{entity}', [BulkActionHandlerInterface::class, 'handleDelete'])->name('delete');
 });
